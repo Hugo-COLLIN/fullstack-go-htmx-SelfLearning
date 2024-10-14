791c5b6bfcd87eaca2b6a935c61627c078ba1173
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-10-14 20:36:23 +0200

    Simple web server - https://youtu.be/8z8VrlVOIyM?t=289

--- main.go
+++ main.go
@@ -2,41 +2,14 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"log"
 	"net/http"
 )
 
-type Todo struct {
-	Id      int
-	Message string
-}
-
 func main() {
-	fmt.Println("Hello World!!!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
 
-	data := map[string][]Todo{
-		"Todos": {
-			Todo{Id: 1, Message: "Buy Milk"},
-		},
-	}
-
-	todosHandler := func(w http.ResponseWriter, r *http.Request) {
-		tpl := template.Must(template.ParseFiles("index.html"))
-		tpl.Execute(w, data)
-	}
-
-	addTodoHandler := func(w http.ResponseWriter, r *http.Request) {
-		msg := r.PostFormValue("message")
-		tpl := template.Must(template.ParseFiles("index.html"))
-		todo := Todo{Id: len(data["Todos"]) + 1, Message: msg}
-		data["Todos"] = append(data["Todos"], todo)
-
-		tpl.ExecuteTemplate(w, "todo-list-element", todo)
-	}
-
-	http.HandleFunc("/", todosHandler)
-	http.HandleFunc("/add-todo", addTodoHandler)
-
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	fmt.Println("Started http server on port :8080")
+	http.ListenAndServe(":8080", nil)
 }
