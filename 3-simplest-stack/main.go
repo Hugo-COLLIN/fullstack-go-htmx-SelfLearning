1a1acd4fad0cb231a224344f24c9400ef2c8156e
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-10-14 23:04:52 +0200

    Sqlite Database and ToDo routes - https://youtu.be/jeT8m7490Pg?t=485

--- 3-simplest-stack/main.go
+++ 3-simplest-stack/main.go
@@ -3,16 +3,39 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func main() {
+	InitDatabase()
+	defer DB.Close() // defer enable to execute instruction when the function exits - here close the database connection
+
 	e := gin.Default()
 	e.LoadHTMLGlob("templates/*")
 
 	e.GET("/", func(c *gin.Context) {
+		todos := ReadToDoList()
 		c.HTML(http.StatusOK, "index.html", gin.H{
-			"name": "Hugo",
+			"todos": todos,
 		})
+	})
+
+	e.POST("/todos", func(c *gin.Context) {
+		title := c.PostForm("title")
+		status := c.PostForm("status")
+		id, _ := CreateToDo(title, status)
+
+		c.HTML(http.StatusOK, "task.html", gin.H{
+			"id":     id,
+			"title":  title,
+			"status": status,
+		})
+	})
+
+	e.DELETE("/todos/:id", func(c *gin.Context) {
+		param := c.Param("id")
+		id, _ := strconv.ParseInt(param, 10, 64)
+		DeleteToDo(id)
 	})
 
 	e.Run(":8080")
